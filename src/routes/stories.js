a86a407bfedcd3655327875c1534176dc182490c
Huynh Truong <huynhphuoctruongk17086@gmail.com>
2022-07-27 12:19:51 +0700

    update stories controller

--- src/routes/stories.js
+++ src/routes/stories.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const StoryController = require('../controllers/storyController')
+const StoryController = require('../controllers/StoryController')
 const router = express.Router()
 
 router.get('/', StoryController.getAllStory)
