4a3e7dd10d9eedd3d3d158e8f3f1acb356579376
Huynh Truong <huynhphuoctruongk17086@gmail.com>
2022-08-17 14:30:11 +0700

    update send email

--- src/config/sendMail.js
+++ src/config/sendMail.js
@@ -1,10 +1,9 @@
 const nodemailer = require('nodemailer')
 const { OAuth2Client } = require('google-auth-library')
 
-const OAUTH_PLAYGROUND = 'https://developers.google.com/oauthplayground'
-
 const sendEmail = async (to, url, txt = 'Hãy xác minh email của bạn tại đây') => {
     console.log(url)
+    const OAUTH_PLAYGROUND = 'https://developers.google.com/oauthplayground'
     const CLIENT_ID = `${process.env.CLIENT_ID}`
     const CLIENT_SECRET = `${process.env.CLIENT_SECRET}`
     const REFRESH_TOKEN = `${process.env.REFRESH_TOKEN}`
@@ -33,14 +32,14 @@ const sendEmail = async (to, url, txt = 'Hãy xác minh email của bạn tại 
             subject: 'blog',
             html: `
                     <div style="max-width: 700px; margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
-                    <h2 style="text-align: center; text-transform: uppercase;color: teal;">Hi, mình là Athetics Huynh</h2>
+                    <h2 style="text-align: center; text-transform: uppercase;color: teal;">Hi, mình là Athetics Huynh, Mình là Admin của trang web H.Blogs</h2>
                     <p>Chúc mừng bạn đã đăng ký thành công. Bạn có thể sử dụng Blog của mình.
                         Hãy nhấn vào nút phía dưới để xác nhận email của bạn.
                     </p>
                     
                     <a href=${url} style="background: crimson; text-decoration: none; color: white; padding: 10px 20px; margin: 10px 0; display: inline-block;" target="_blank">${txt}</a>
                 
-                    <p>Nếu nhấn vào nút không được thì bạn hãy click vào đường link phía dưới này.</p>
+                    <p>Nếu nhấn vào nút không được thì bạn hãy click vào đường link phía bên dưới này.</p>
                 
                     <div>${url}</div>
                     </div>
